d25c941b8679f14c7246404c3cab52a5914699e6
daniilusacev <2893318@gmail.com>
2023-05-04 00:31:43 +0300

    task2 done

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
-/* task1 */
+
 public class Main {
 
     public static void main(String[] args){
+        /* task1 */
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        /* task2 */
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend * 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
 
     }
 }
